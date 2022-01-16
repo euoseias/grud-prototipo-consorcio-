10023b8a0809da3dd30b31727ec2a60645f8d20a
euoseias <oseiasramossouza@gmail.com>
2022-01-16 10:31:32 -0300

    sql banco de dados

---
+++ prototipo.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12-Out-2020 às 20:05
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `prototipo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `consorcio`
+--
+
+CREATE TABLE `consorcio` (
+  `id_consorcio` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `cota` float NOT NULL,
+  `valor` float NOT NULL,
+  `situacao` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
